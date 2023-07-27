727ac67097ecf58350f0cbac6547813ae97d9cc2
jiyun <wldbs8563@gmail.com>
2023-07-27 17:40:38 +0900

    pawin0727

--- script/script.js
+++ script/script.js
@@ -11,13 +11,13 @@ m_nav_open.style.display = 'none';
 //객체.속성 style.display = 'none'
 //2. 햄버거 메뉴 클릭 시 모바일 메뉴 보이기
 m_nav.addEventListener('click',function(){
-    m_nav_open.style.display = 'block'
+    m_nav_open.style.display = 'block';
 })
 //객체.이벤트
 //객체 = 햄버거메뉴m_nav, 모바일메뉴m_nav_open
 //3. x버튼 클릭 시 모바일 메뉴 숨기기
 close.addEventListener('click',function(){
-    m_nav_open.style.display = 'none'
+    m_nav_open.style.display = 'none';
 })
 //x버튼close, 모바일메뉴m_nav_open
 
