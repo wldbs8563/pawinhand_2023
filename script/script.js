bdcf5efb475c93adecf067df9e3e0afc3859b102
jiyun <wldbs8563@gmail.com>
2023-07-31 17:37:39 +0900

    반응형웹슬라이드

--- script/script.js
+++ script/script.js
@@ -25,11 +25,12 @@ close.addEventListener('click',function(){
 //const 변수생성 = new Swiper('적용대상',{속성:값, 속성:값});
 const pawin_slide = new Swiper('#pawin_slide',{
     autoplay:{
-        delay:1000,//슬라이드 간격(밀리초) 기본3초
+        delay:10000,//슬라이드 간격(밀리초) 기본3초
         disableOnInteraction:false,//버튼 클릭 후 자동재생 유지
     },//자동재생
     loop:true,//반복재생 (기본값 false)
-    effect:'fade',//제자리 변경 슬라이드 효과
+    direction:'horizontal',
+    //effect:'fade',//제자리 변경 슬라이드 효과
     //direction:'vertical'//위에서 아래로 재생
     navigation: {//이전, 다음 내비게이션 연결
         //next,prev 객체 연결시 부모를 안적으면
@@ -37,15 +38,15 @@ const pawin_slide = new Swiper('#pawin_slide',{
         //개별인식 가능한 부모이름을 반드시 앞에 먼저 작성한다
         nextEl: '#pawin_slide .swiper-button-next',
         prevEl: '#pawin_slide .swiper-button-prev',
-    },
+    }
 })
-const pawin_slide2 = new Swiper('#pawin_slide2',{
-    autoplay:{delay:10000},
-    effect:'fade',
-    direction:'vertical',
-    loop:true,
-    navigation: {
-        nextEl: '#pawin_slide2 .swiper-button-next',
-        prevEl: '#pawin_slide2 .swiper-button-prev',
-    },
-})
+// const pawin_slide2 = new Swiper('#pawin_slide2',{
+//     autoplay:{delay:10000},
+//     effect:'fade',
+//     direction:'vertical',
+//     loop:true,
+//     navigation: {
+//         nextEl: '#pawin_slide2 .swiper-button-next',
+//         prevEl: '#pawin_slide2 .swiper-button-prev',
+//     },
+// })
